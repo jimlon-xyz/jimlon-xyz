b1aa374f950b65873ee3a8610e4f00db2527220a
jimlon-xyz <hello@jimlon.xyz>
2023-03-13 17:14:08 +0800

    initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
 module.exports = defineConfig({
 	publicPath: './',
     assetsDir: 'assets',
@@ -20,8 +22,13 @@ module.exports = defineConfig({
             args[0].title = '吉米网 | 走进一个Web3时代'
             return args
         })
+		
 		config.plugin('copy').tap(args => {
-			
+			args[0].patterns.push({
+				from: path.resolve('CNAME'),
+				to: path.resolve('docs')
+			})
+			return args
 		})
 	}
 })
